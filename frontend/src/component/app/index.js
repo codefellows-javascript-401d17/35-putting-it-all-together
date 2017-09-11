e41852bea31cc891cd157f6de298b813e754c124
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-09-11 16:36:14 -0700

    added avatar and icon components

--- frontend/src/component/app/index.js
+++ frontend/src/component/app/index.js
@@ -1,27 +1,19 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
+
+import NavBar from
 import LandingContainer from '../landing-container';
 import DashboardContainer from '../dashboard-container';
 
 class App extends React.Component {
   render() {
     return(
-      <div className='cfGram'>
+      <div className='app'>
         <BrowserRouter>
           <section>
-            <header>
-              <h1>cfgram</h1>
-              <nav>
-                <ul>
-                  <li><Link to='/welcome/signup'>signup</Link></li>
-                  <li><Link to='/welcome/login'>login</Link></li>
-                  <li><Link to='/settings'>settings</Link></li>
-                </ul>
-              </nav>
-            </header>
-            <Route path='/welcome/:auth' component={LandingContainer} />
-            <Route exact path='/settings' component={SettingsContainer} />
+
+
           </section>
         </BrowserRouter>
       </div>
