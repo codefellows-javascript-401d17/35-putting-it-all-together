b200c0566bfe4c0fefce739c07f80f93d0a7b37f
Allan Liebold <allan.liebold@gmail.com>
2017-09-12 00:03:22 -0700

    creates photo actions

--- frontend/src/action/profile-actions.js
+++ frontend/src/action/profile-actions.js
@@ -18,6 +18,28 @@ export const profileCreateRequest = (profile) => (dispatch, getState) => {
   .attach('avatar', profile.avatar)
   .then(res => {
     dispatch(profileCreate(res.body));
+    return res;
+  })
+}
+
+export const userProfileUpdateRequest = (profile) => (dispatch, getState) => {
+  let {auth} = getState()
+  return superagent.put(`${__API_URL__}/profiles/${profile._id}`)
+  .set('Authorization'), `Bearer ${auth}`)
+  .field('bio', profile.bio)
+  .attach('avatar', profile.avatar)
+  .then(res => {
+    dispatch(userProfileCreate(res.body));
+    return res;
+  })
+}
+
+export const userProfileFetchRequest = () => (dispatch, getState) => {
+  let {auth} = getState();
+  return superagent.get(`${__API_URL__}/profiles/user`)
+  .set('Authorization', `Bearer ${auth}`)
+  .then(res => {
+    dispatch(userProfileCreate(res.body))
     return res; 
   })
 }
