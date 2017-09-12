71cc1284803ed300d3c7060f9c49879cba1e1488
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-09-11 17:20:10 -0700

    finished app

--- frontend/src/component/app/index.js
+++ frontend/src/component/app/index.js
@@ -2,18 +2,26 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
 
-import NavBar from
-import LandingContainer from '../landing-container';
-import DashboardContainer from '../dashboard-container';
+import NavBar from '../navbar/index';
+import * as util from '../../lib/util';
+import LandingContainer from '../landing-container/index';
+import DashboardContainer from '../dashboard-container/index';
+import Settingscontainer from '../settings-container/index';
+import {tokenSet} from '../../action/auth-actions';
+import {profileFetchRequest} from '../../action/profile-actions'
 
 class App extends React.Component {
+
   render() {
     return(
       <div className='app'>
         <BrowserRouter>
           <section>
-
-
+            <Route path='*' component={Navbar} />
+            <Route exact path='/welcome/:auth' component={LandingContainer} />
+            <Route exact path='/settings' component={SettingsContainer} />
+            <Route exact path='/dashboard' component={DashboardContainer} />
+            <Route exact path='/' component={DashboardContainer} />
           </section>
         </BrowserRouter>
       </div>
@@ -21,4 +29,13 @@ class App extends React.Component {
   }
 }
 
-export default App;
+let mapStateToProps = (state) => ({
+  profile: state.profile,
+})
+
+let mapDispatchToProps = (dispatch) => ({
+  tokenSet: (token) => dispatch(tokenSet(token)),
+  profileFetch: () => dispatch(profileFetchRequest()),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
