9cd2a95a980ba53213bafbfc4b4bdc4e6799d1e7
Allan Liebold <allan.liebold@gmail.com>
2017-09-12 07:35:51 -0700

    fixes typo in profile-actions

--- frontend/src/action/profile-actions.js
+++ frontend/src/action/profile-actions.js
@@ -25,7 +25,7 @@ export const profileCreateRequest = (profile) => (dispatch, getState) => {
 export const profileUpdateRequest = (profile) => (dispatch, getState) => {
   let {auth} = getState()
   return superagent.put(`${__API_URL__}/profiles/${profile._id}`)
-  .set('Authorization'), `Bearer ${auth}`)
+  .set('Authorization', `Bearer ${auth}`)
   .field('bio', profile.bio)
   .attach('avatar', profile.avatar)
   .then(res => {
