282f785fbf4b18b6a836fb4d3f8583935cf37a02
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-09-12 15:36:56 -0700

    update auth router with oauth route

--- backend/src/middleware/router-auth.js
+++ backend/src/middleware/router-auth.js
@@ -7,6 +7,40 @@ import {basicAuth} from './parser-auth.js'
 import {log, daysToMilliseconds} from '../lib/util.js'
 
 export default new Router()
+.get('/oauth/google/code', (req, res, next) => {
+  if (!req.query.code) {
+    res.redirect(process.env.CLIENT_URL);
+  } else {
+    superagent.post('https://www.googleapis.com/oauth2/v4/token')
+    .type('form')
+    .send({
+      code: req.query.code,
+      grant_type: 'authorization_code',
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      client_secret: process.env.GOOGLE_CLIENT_SECRET,
+      redirect_uri: `${process.env.API_URL}/oauth/google/code`
+    })
+    .then(response => {
+      console.log('POST: oauth2/v4/token', response.body);
+      return superagent.get('https://www.googleapis.com/plus/v1/people/me/openIdConnect')
+      .set('Authorization', `Bearer ${response.body.access_token}`)
+    })
+    .then(response => {
+      console.log('GET: /people/me/openIdConnect', response.body);
+      return User.handleOAUTH(response.body);
+    })
+    .then(user => user.tokenCreate())
+    .then( token => {
+      console.log('my oauth token:', token);
+      res.cookie('X-Slugchat-Token', token);
+      res.redirect(process.env.CLIENT_URL);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.redirect(process.env.CLIENT_URL);
+    })
+  }
+})
 .post('/signup', parserBody, (req, res, next) => {
   log('__ROUTE__ POST /signup')
 
